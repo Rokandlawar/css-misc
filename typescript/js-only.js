22cd322cb3b87054355e3b762764e7d90db16040
Rohan <rokandlawar.rohan59@gmail.com>
2022-12-03 09:42:17 +0530

    basic typescript ex

--- typescript/js-only.js
+++ typescript/js-only.js
@@ -1,11 +1,11 @@
-const button = document.querySelector("button")
-const input1 = document.getElementById("num1")
-const input2 = document.getElementById("num2")
+// const button = document.querySelector("button")
+// const input1 = document.getElementById("num1")
+// const input2 = document.getElementById("num2")
 
-function sum(num1, num2) {
-    return num1 + num2
-}
+// function sum(num1, num2) {
+//     return num1 + num2
+// }
 
-button.addEventListener("click",function(){
-    console.log(sum(input1,input2))
-})
+// button.addEventListener("click",function(){
+//     console.log(sum(input1,input2))
+// })
